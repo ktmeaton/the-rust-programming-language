ee067958191954d2e3329213b0e7d4c2319c1aee
Katherine Eaton <ktmeaton@gmail.com>
2023-04-29 08:49:33 -0600

    chapter 02: finish

---
+++ chapter-02/guessing_game/src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    // Randomly generate a secret number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");   
+
+    // Prompt for the user to guess the number
+    loop {
+        
+        let mut guess = String::new();
+        println!("Guess the secret number:");
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");    
+
+        // Parse and convert user input.
+        // let guess: u32 = guess.trim().parse().expect("Guess the secret number:");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(error) => {
+                println!("Error: {}. Please try again.", error);
+                continue;
+            },
+        };
+
+        // Compare guess to secret number
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
